08e4596e21801a9720c4c31cdcf8fbb60f0aae9d
Gustavo Teodoro <109152947+gusteodoro04@users.noreply.github.com>
2025-09-22 20:33:03 -0300

    atualização da atividade 8

---
+++ Exercicio8.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using bibliotecamatriz;
+class matriz8
+{
+    static int raiosdomesmolocal(int[,] matriz)
+    {
+        int linhas = matriz.GetLength(0);
+        int colunas = matriz.GetLength(1);
+        int contador = 0;
+        for (int i = 0; i < linhas; i++)
+            {
+            for (int j = 0; j < colunas; j++)
+            {
+                if (matriz[i, j] > 1)
+                {
+                    contador++;
+                }
+            }
+        }
+     return contador;
+    }
+    static void Main()
+    {
+        int x, y, numerosderaios;
+        int linhas, colunas;
+        Console.WriteLine("Qual a quantiade de linhas e colunas?");
+        linhas = int.Parse(Console.ReadLine());
+        colunas = int.Parse(Console.ReadLine());
+        int[,] mapa = new int[linhas, colunas];
+        mybiblioteca.mostraMatriz(mapa);
+        Console.Write("Quantos raios caíram? ");
+        numerosderaios = int.Parse(Console.ReadLine());
+        for (int i = 0; i < numerosderaios; i++)
+        {
+            Console.WriteLine("Digite a cordenada em que eses raios cairam:");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+            mapa[x, y]++;
+
+        }
+        int raiosdemesmplocal = raiosdomesmolocal(mapa);
+        mybiblioteca.mostraMatriz(mapa);
+        Console.WriteLine($"O número de ocorrencias de raio de mesmo local foram {raiosdemesmplocal}"); 
+    }
+}
